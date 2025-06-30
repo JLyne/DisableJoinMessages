d42c44c932d13a425d7c427cc1ba1c84c91f4cb0
James Lyne <jim+github@not-null.co.uk>
2025-06-30 23:55:29 +0100

    1.21.7

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,3 @@
-import net.minecrell.pluginyml.bukkit.BukkitPluginDescription
-import net.minecrell.pluginyml.paper.PaperPluginDescription
-
 plugins {
     java
     alias(libs.plugins.pluginYmlPaper)
